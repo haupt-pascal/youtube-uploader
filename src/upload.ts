e18d3b9e69bc95704626450403432a3b534cb13d
Pascal Haupt <75530910+haupt-pascal@users.noreply.github.com>
2023-07-19 20:33:42 +0200

    diasbled stealth

--- src/upload.ts
+++ src/upload.ts
@@ -12,10 +12,10 @@ import puppeteer from 'puppeteer-extra'
 import { PuppeteerNodeLaunchOptions, Browser, Page, ElementHandle } from 'puppeteer'
 import fs from 'fs-extra'
 import path from 'path'
-const StealthPlugin = require('puppeteer-extra-plugin-stealth')()
-StealthPlugin.enabledEvasions.delete('iframe.contentWindow')
-StealthPlugin.enabledEvasions.delete('navigator.plugins')
-puppeteer.use(StealthPlugin)
+//const StealthPlugin = require('puppeteer-extra-plugin-stealth')()
+//StealthPlugin.enabledEvasions.delete('iframe.contentWindow')
+//StealthPlugin.enabledEvasions.delete('navigator.plugins')
+//puppeteer.use(StealthPlugin)
 
 const maxTitleLen = 100
 const maxDescLen = 5000
