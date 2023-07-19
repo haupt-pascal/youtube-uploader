f700cdb4428e1ac49e31b49433dad99b63dfd8f8
Pascal Haupt <75530910+haupt-pascal@users.noreply.github.com>
2023-07-19 20:29:13 +0200

    next test

--- src/upload.ts
+++ src/upload.ts
@@ -30,7 +30,8 @@ let cookiesFilePath: string
 
 const invalidCharacters = ['<', '>']
 
-const uploadURL = 'https://www.youtube.com/upload?persist_gl=1&gl=US&persist_hl=1&hl=en'
+//const uploadURL = 'https://www.youtube.com/upload?persist_gl=1&gl=US&persist_hl=1&hl=en'
+const uploadURL = 'https://youtube.com/upload'
 const homePageURL = 'https://www.youtube.com/?persist_gl=1&gl=US&persist_hl=1&hl=en'
 
 const defaultMessageTransport: MessageTransport = {
