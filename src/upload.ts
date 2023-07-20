623e5ea6f73ff0223ceec188f954726094f27f1f
Pascal Haupt <75530910+haupt-pascal@users.noreply.github.com>
2023-07-20 16:48:45 +0200

    changed domain

--- src/upload.ts
+++ src/upload.ts
@@ -31,7 +31,8 @@ let cookiesFilePath: string
 const invalidCharacters = ['<', '>']
 
 //const uploadURL = 'https://www.youtube.com/upload?persist_gl=1&gl=US&persist_hl=1&hl=en'
-const uploadURL = 'https://youtube.com/upload'
+//const uploadURL = 'https://youtube.com/upload'
+const uploadURL = 'nowsecure.nl/'
 const homePageURL = 'https://www.youtube.com/?persist_gl=1&gl=US&persist_hl=1&hl=en'
 
 const defaultMessageTransport: MessageTransport = {
