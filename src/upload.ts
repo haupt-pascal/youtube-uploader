f9ac055ce43dd43fbdfa89cfc524d447f1fac098
Pascal Haupt <75530910+haupt-pascal@users.noreply.github.com>
2023-07-20 17:51:37 +0200

    T

--- src/upload.ts
+++ src/upload.ts
@@ -33,8 +33,9 @@ let cookiesFilePath: string
 const invalidCharacters = ['<', '>']
 
 //const uploadURL = 'https://www.youtube.com/upload?persist_gl=1&gl=US&persist_hl=1&hl=en'
-const uploadURL = 'https://youtube.com/upload'
+//const uploadURL = 'https://youtube.com/upload'
 //const uploadURL = 'https://nowsecure.nl/'
+const uploadURL = 'https://bot.sannysoft.com'
 const homePageURL = 'https://www.youtube.com/?persist_gl=1&gl=US&persist_hl=1&hl=en'
 
 const defaultMessageTransport: MessageTransport = {
